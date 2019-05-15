03006891bb115ec793f29df3a79b526b76b625c7
mansuiki <mansuiki@gmail.com>
2019-05-15 21:16:23 +0900

    OOP 기본 공부
    과제 있음!!!
    달력 만들기!

---
+++ src/OOP_Start.java
@@ -0,0 +1,26 @@
+public class OOP_Start {
+    public static void main(String[] args) {
+        OOP_Human mansuiki = new OOP_Human(180, 80, 100);
+        OOP_Human jobs = new OOP_Human();
+
+        System.out.println("mansuiki sleep status : " + mansuiki.isSleep);
+        System.out.println("jobs sleep status : " + jobs.isSleep);
+
+        mansuiki.sleep();
+        jobs.sleep();
+
+        mansuiki.homework();
+        mansuiki.eat();
+
+        jobs.execerise();
+
+        if (jobs.hp > mansuiki.hp) {
+            System.out.println("jobs Win!");
+        } else {
+            System.out.println("mansuiki Win!");
+        }
+
+    }
+
+
+}
